10a193a6f5018f33b7b7ead2b8a7417336a5d852
Juan Villegas <juanvj0425@gmail.com>
2023-08-28 08:17:20 -0500

    fix: issues with patient appointment

--- src/api/patient/patient.controller.ts
+++ src/api/patient/patient.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { AuthRequest, OptionRequest } from '../../auth/auth.types';
-import { getPatientAppintmentByID, getPatientById, updatePatient } from './patient.service';
+import { getPatientById, updatePatient } from './patient.service';
 import { Patient } from '@prisma/client';
 
 export async function updatePatientHandler(req: OptionRequest, res: Response) {
@@ -22,24 +22,24 @@ export async function updatePatientHandler(req: OptionRequest, res: Response) {
   }
 }
 
-export async function getPatientAppointmentByIDHandler(req: OptionRequest, res: Response) {
+// export async function getPatientAppointmentByIDHandler(req: OptionRequest, res: Response) {
 
-  const { patient: { id: patientID } } = req.user
+//   const { patient: { id: patientID } } = req.user
 
-  try {
+//   try {
 
-    if (!patientID) {
-      return res.status(401).send('You are not patient');
-    }
+//     if (!patientID) {
+//       return res.status(401).send('You are not patient');
+//     }
 
-    const patientAppointment = await getPatientAppintmentByID(patientID)
+//     const patientAppointment = await getPatientAppintmentByID(patientID)
 
-    res.status(200).json({ patientAppointment })
+//     res.status(200).json({ patientAppointment })
 
-  } catch (error) {
-    res.status(500).json({ error })
-  }
-}
+//   } catch (error) {
+//     res.status(500).json({ error })
+//   }
+// }
 
 export async function getPatientSingle(req: OptionRequest, res: Response) {
   const { id } = req.user
